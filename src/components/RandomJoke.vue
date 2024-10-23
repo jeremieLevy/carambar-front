07c40e886f860a24457a82831a659be40c2f5c2a
Jeremie Levy <jeremylevy.131290@gmail.com>
2024-10-23 19:38:52 +0200

    created vue app

---
+++ src/components/RandomJoke.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h1>Carambar Jokes</h1>
+    <p v-if="joke">{{ joke.content }}</p>
+    <!-- <p v-else>Chargement...</p> -->
+    <button @click="fetchRandomJoke">Joke</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      joke: ''
+    }
+  },
+  methods: {
+    async getRandomJoke() {
+      try {
+        const response = await fetch('https://carambar-api.onrender.com/blagues/random')
+        const data = await response.json()
+        this.joke = data.content;
+      } catch (error) {
+        console.error('Error fetching joke:', error)
+      }
+    }
+  }
+}
+</script>
